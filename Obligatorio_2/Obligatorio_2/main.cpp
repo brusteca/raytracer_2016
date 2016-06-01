5b73d595413aa5bfea44529d16953eda7ed397a0
bedelia <bedelia@ens.local>
2016-06-01 18:56:42 -0300

    ppm

--- Obligatorio_2/Obligatorio_2/main.cpp
+++ Obligatorio_2/Obligatorio_2/main.cpp
@@ -1,6 +1,11 @@
+#define byte unsigned char
+
+
 #include "pugixml-1.7/src/pugixml.hpp"
 
 using namespace std;
+
+
 
 int main() {
 	// Leer archivo xml y construir shapes
@@ -11,4 +16,33 @@ int main() {
 	for (pugi::xml_node_iterator nodo = doc.begin(); nodo != doc.end(); nodo++) {
 		
 	}
+
+	int width, height;
+	width = height = 100;
+	byte * red_value = new byte[width*height];
+	byte * green_value = new byte[width*height];
+	byte * blue_value = new byte[width*height];
+
+	for (int y = 0; y<height; y++)
+		for (int x = 0; x<width; x++)
+		{
+			red_value[y*width + height] = 0;
+			green_value[y*width + height] = 255;
+			blue_value[y*width + height] = 0;
+		}
+
+	//imprimir imagen
+	FILE *f;
+	errno_t err = fopen_s(&f, "out.ppm", "w+");;
+	fprintf(f, "P6\n%i %i 255\n", width, height);
+	for (int y = 0; y<height; y++)
+		for (int x = 0; x<width; x++)
+		{
+			fputc(red_value[y*width + height], f);   // 0 .. 255
+			fputc(green_value[y*width + height], f); // 0 .. 255
+			fputc(blue_value[y*width + height], f);  // 0 .. 255
+		}
+	fclose(f);
+
+
 }

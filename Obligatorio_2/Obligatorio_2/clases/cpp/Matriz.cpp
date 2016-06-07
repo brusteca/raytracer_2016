c5bda93be39452ccfaa3a76142eacc56da4aff71
bedelia <bedelia@ens.local>
2016-06-07 18:10:23 -0300

    Matriz avanzada

--- Obligatorio_2/Obligatorio_2/clases/cpp/Matriz.cpp
+++ Obligatorio_2/Obligatorio_2/clases/cpp/Matriz.cpp
@@ -40,7 +40,27 @@ Punto* Matriz::resolverSistema() {
 	// Escalerizar la matriz 
 	// Recorrer columnas y ejecutar escalerización
 	for (int j = 0; j < 3; j++){
-	
+		// Si estamos en i == j entonces ver si es cero
+		if (filas[j][j] == 0) {
+			// Si lo es, debemos hacer swap de fila con alguna que no sea cero
+			int k = j+1;
+			while ((k < 3)&&(filas[k][j] != 0))
+				k++;
+			// Si todas por debajo son cero, estamos en problemas.
+			if (k == 3)
+				return NULL;
+			// Sino, hacemos swap de filas
+			float* aux = filas[j];
+			filas[j] = filas[k];
+			filas[k] = filas[j];
+		}
+		// Recorrer filas inferiores a la altura de la diagonal para escalerizar
+		for (int i = j + 1; i < 3; i++) {
+			// Para toda fila cuyo elemento de esta columna no sea cero, modificar para que sea cero
+			if (filas[i][j] != 0) {
+				float* aux = new float[3];
+			}
+		}
 	}
 
 }
